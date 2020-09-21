542fbe2e93cd7a3406818b7b4aaf0f8a3cf2bc44
gyoun-im <gyoun.im@student.uts.edu.au>
2020-09-21 23:46:44 +1000

    added some audio assets and began pacman movement

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject pacman;
+
+    private Tweener tweener;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tweener = GetComponent<Tweener>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pacman.transform.position.x == -28 && pacman.transform.position.y == 26)
+        {
+            tweener.addTween(pacman.transform, pacman.transform.position, new Vector3(-19.0f, 26.0f, 0.0f), 3.0f);
+        }
+        if (pacman.transform.position.x == -19 && pacman.transform.position.y == 26)
+        {
+            tweener.addTween(pacman.transform, pacman.transform.position, new Vector3(-19.0f, 19.0f, 0.0f), 3.0f);
+        }
+        if (pacman.transform.position.x == -19 && pacman.transform.position.y == 19)
+        {
+            tweener.addTween(pacman.transform, pacman.transform.position, new Vector3(-28.0f, 19.0f, 0.0f), 3.0f);
+        }
+        if (pacman.transform.position.x == -28 && pacman.transform.position.y == 19)
+        {
+            tweener.addTween(pacman.transform, pacman.transform.position, new Vector3(-28.0f, 26.0f, 0.0f), 3.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/Tweener.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tweener : MonoBehaviour
+{
+    private Tween activeTween;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(activeTween.Target.position, activeTween.EndPos) > 0.1f)
+        {
+            float number = (Time.time - activeTween.StartTime) / activeTween.Duration;
+            float timeFraction = activeTween.Duration * number * number * number;
+            activeTween.Target.position = Vector3.Lerp(activeTween.StartPos, activeTween.EndPos, timeFraction);
+        }
+    }
+
+    public void addTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration)
+    {
+        if (activeTween == null)
+        {
+            activeTween = new Tween(targetObject, startPos, endPos, Time.time, duration);
+        }
+    }
+}
