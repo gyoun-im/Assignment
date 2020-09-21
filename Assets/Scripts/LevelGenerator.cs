ad6b2d42a8660da947fd58dca2109ccd0ad28361
gyoun-im <gyoun.im@student.uts.edu.au>
2020-09-21 19:39:19 +1000

    load all quarters of level

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -12,7 +12,7 @@ public class LevelGenerator : MonoBehaviour
     public GameObject normalPellet;
     public GameObject powerPellet;
 
-    int [,] leveMap = 
+    private int [,] levelMap = 
         {
          {1,2,2,2,2,2,2,2,2,2,2,2,2,7},
          {2,5,5,5,5,5,5,5,5,5,5,5,5,4},
@@ -31,57 +31,23 @@ public class LevelGenerator : MonoBehaviour
          {0,0,0,0,0,0,5,0,0,0,4,0,0,0},
         };
 
-    int xPos;
-    int yPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        yPos = 10;
-
-        for (int i = 0; i < 15; ++i)
+        createLevelQuarter(levelMap, -30.0f, 28.0f);
+        flipArray(1);
+        createLevelQuarter(levelMap, -3.4f, 28.0f);
+        flipArray(2);
+        for (int i = 0; i < 14; ++ i)
         {
-            xPos = -10;
-            for (int j = 0; j < 14; ++j)
-            {
-                if (leveMap[i, j] == 1)
-                {
-                    createOutsideCorner(xPos, yPos, 0);
-                }
-
-                if (leveMap[i, j] == 2)
-                {
-                    createOutsideWall(xPos, yPos, 0);
-                }
-
-                if (leveMap[i, j] == 3)
-                {
-                    createInsideCorner(xPos, yPos, 0);
-                }
-
-                if (leveMap[i, j] == 4)
-                {
-                    createInsideWall(xPos, yPos, 0);
-                }
-
-                if (leveMap[i, j] == 5)
-                {
-                    createNormalPellet(xPos, yPos, 0);
-                }
-
-                if (leveMap[i, j] == 6)
-                {
-                    createPowerPellet(xPos, yPos, 0);
-                }
-
-                if (leveMap[i, j] == 7)
-                {
-                    createTJunction(xPos, yPos, 0);
-                }
-                xPos += 2;
-            }
-            yPos -= 2;
+            levelMap[0, i] = 0;
         }
+        createLevelQuarter(levelMap, -3.4f, 1.4f);
+        flipArray(1);
+        createLevelQuarter(levelMap, -30.0f, 1.4f);
+
+
     }
 
     // Update is called once per frame
@@ -90,39 +56,157 @@ public class LevelGenerator : MonoBehaviour
         
     }
 
-
-    private void createOutsideCorner(int xPos, int yPos, int rotation)
+    private void createLevelQuarter(int [,] array, float x, float y)
     {
-        Instantiate(outsideCorner, new Vector3(xPos, yPos, 0), Quaternion.identity);
+        float yPos = y;
+        for (int i = 0; i < 15; ++i)
+        {
+            float xPos = x;
+            for (int j = 0; j < 14; ++j)
+            {
+                if (levelMap[i, j] == 1)
+                {
+                    createOutsideCorner(xPos, yPos, findRotation(i, j));
+                }
+
+                if (levelMap[i, j] == 2)
+                {
+                    createOutsideWall(xPos, yPos, findRotation(i, j));
+                }
+
+                if (levelMap[i, j] == 3)
+                {
+                    createInsideCorner(xPos, yPos, findRotation(i, j));
+                }
+
+                if (levelMap[i, j] == 4)
+                {
+                    createInsideWall(xPos, yPos, findRotation(i, j));
+                }
+
+                if (levelMap[i, j] == 5)
+                {
+                    createNormalPellet(xPos, yPos, 0);
+                }
+
+                if (levelMap[i, j] == 6)
+                {
+                    createPowerPellet(xPos, yPos, 0);
+                }
+
+                if (levelMap[i, j] == 7)
+                {
+                    createTJunction(xPos, yPos, findRotation(i, j));
+                }
+                xPos += 1.9f;
+            }
+            yPos -= 1.9f;
+        }
     }
 
-    private void createOutsideWall(int xPos, int yPos, int rotation)
+
+
+    private void createOutsideCorner(float xPos, float yPos, int rotation)
     {
-        Instantiate(outsideWall, new Vector3(xPos, yPos, 0), Quaternion.identity);
+        Instantiate(outsideCorner, new Vector3(xPos, yPos, 0), Quaternion.Euler(0, 0, rotation));
     }
 
-    private void createInsideCorner(int xPos, int yPos, int rotation)
+    private void createOutsideWall(float xPos, float yPos, int rotation)
     {
-        Instantiate(insideCorner, new Vector3(xPos, yPos, 0), Quaternion.identity);
+        Instantiate(outsideWall, new Vector3(xPos, yPos, 0), Quaternion.Euler(0, 0, rotation));
     }
 
-    private void createInsideWall(int xPos, int yPos, int rotation)
+    private void createInsideCorner(float xPos, float yPos, int rotation)
     {
-        Instantiate(insideWall, new Vector3(xPos, yPos, 0), Quaternion.identity);
+        Instantiate(insideCorner, new Vector3(xPos, yPos, 0), Quaternion.Euler(0, 0, rotation));
     }
 
-    private void createTJunction(int xPos, int yPos, int rotation)
+    private void createInsideWall(float xPos, float yPos, int rotation)
     {
-        Instantiate(tJunction, new Vector3(xPos, yPos, 0), Quaternion.identity);
+        Instantiate(insideWall, new Vector3(xPos, yPos, 0), Quaternion.Euler(0, 0, rotation));
     }
 
-    private void createNormalPellet(int xPos, int yPos, int rotation)
+    private void createTJunction(float xPos, float yPos, int rotation)
     {
-        Instantiate(normalPellet, new Vector3(xPos, yPos, 0), Quaternion.identity);
+        Instantiate(tJunction, new Vector3(xPos, yPos, 0), Quaternion.Euler(0, 0, rotation));
     }
 
-    private void createPowerPellet(int xPos, int yPos, int rotation)
+    private void createNormalPellet(float xPos, float yPos, int rotation)
     {
-        Instantiate(powerPellet, new Vector3(xPos, yPos, 0), Quaternion.identity);
+        Instantiate(normalPellet, new Vector3(xPos, yPos, 0), Quaternion.Euler(0, 0, rotation));
+    }
+
+    private void createPowerPellet(float xPos, float yPos, int rotation)
+    {
+        Instantiate(powerPellet, new Vector3(xPos, yPos, 0), Quaternion.Euler(0, 0, rotation));
+    }
+
+    private int findRotation(int i, int j)
+    {
+        int rotation = 0;
+        
+        return rotation;
+    }
+
+    private bool isHorizontalEdge(int i, int j)
+    {
+        if (i == 0 || i == 14)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private bool isVerticalEdge(int i, int j)
+    {
+        if ((j == 0 || j == 13) && !isHorizontalEdge(i, j))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void flipArray(int mode)
+    {
+        if (mode == 1)
+        {
+            for (int i = 0; i < 15; ++i)
+            {
+                int rowLength = levelMap.GetLength(1) -1;
+                for (int j = 0; j < 6; ++j)
+                {
+                    swapYValue(i, j, rowLength);
+                    --rowLength;
+                }
+            }
+        }
+        if (mode == 2)
+        {
+            for (int i = 0; i < 14; ++i)
+            {
+                int columnLength = levelMap.GetLength(0) - 1; 
+                for (int j = 0; j < 7; ++j)
+                {
+                    swapXValue(i, j, columnLength);
+                    --columnLength;
+                }
+            }
+        }
+    }
+
+    private void swapYValue(int i, int j, int number)
+    {
+        int temp = levelMap[i, j];
+        levelMap[i, j] = levelMap[i, number];
+        levelMap[i, number] = temp;
+
+    }
+
+    private void swapXValue(int i, int j, int number)
+    {
+        int temp = levelMap[j, i];
+        levelMap[j, i] = levelMap[number, i];
+        levelMap[number, i] = temp;
+
     }
 }
